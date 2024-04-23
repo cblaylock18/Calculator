01540314c44c692b671fc3c1675c661ca3aa09da
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2024-04-22 20:13:22 -0400

    Add html required to show calculator including buttons and display. Begin JS to update display on button presses

--- script.js
+++ script.js
@@ -5,28 +5,44 @@ const subtract = (a, b) => parseFloat(a) - parseFloat(b);
 const multiply = (a, b) => parseFloat(a) * parseFloat(b);
 const divide = (a, b) => parseFloat(a) / parseFloat(b);
 
-// input variables
+// input variables, dummy values for now
 let firstNum = 3;
 let secondNum = 5;
 let operator = "+";
 
+// operate on a pair of numbers
 const operate = (firstNum, secondNum, operator) => {
     switch (operator) {
         case "+":
             return add(firstNum, secondNum);
-            break;
         case "-":
             return subtract(firstNum, secondNum);
-            break;
         case "*":
             return multiply(firstNum, secondNum);
-            break;
         case "/":
             return divide(firstNum, secondNum);
-            break;
         default:
             console.log("Error"); //to be updated
     }
 };
 
+// select relevant html elements
+const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".add-to-display");
+
+// add event listener for input button presses
+numbers.forEach((number) => {
+    number.addEventListener("click", () => displayNumber(number.textContent));
+});
+
+// populate the display
+const displayNumber = (number) => {
+    if (number === "+/-") {
+        display.textContent = multiply(display.textContent, -1);
+    } else {
+        display.textContent = number;
+    }
+};
+// next, get display to update with more than one digit numbers
+// be sure to store display variable somewhere, will need it to operate
 console.log(operate(firstNum, secondNum, operator));
